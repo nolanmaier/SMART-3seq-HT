26f9bab30798e73a85e95a8a09eabce7760069ed
nolanmaier <91697527+nolanmaier@users.noreply.github.com>
2022-10-14 09:46:35 -0400

    Add files via upload

---
+++ smart3seq_summary.sh
@@ -0,0 +1,194 @@
+#! /bin/bash
+
+#################################################
+# SMART3SEQ SUMMARY SCRIPT
+# this script runs summary tools after all the individual fastq files have been processed
+#################################################
+
+#################################################
+# SETUP
+#################################################
+
+# define a function to display help information for the script
+Help()
+{
+  echo 'Syntax: smart3seq_summary.sh -o [output directory] -g [gtf reference file]'
+}
+
+# get the input options from the command line arguments
+while getopts ":o:g:h" flag; do
+  case "${flag}" in
+    o) OUTDIR=${OPTARG} ;;
+    g) GTFFILE=${OPTARG} ;;
+    h) Help; exit ;;
+    :) echo "$0: Must supply an argument to -${OPTARG}"; exit 1;;
+    \?) echo "Invalid option: -${OPTARG}"; Help; exit 1 ;;
+  esac
+done
+shift "$(($OPTIND -1))"
+
+
+# "SECONDS" is a special bash variable that tracks the time
+SECONDS=0
+
+# print out the command submitted to initialize this script
+echo "smart3seq summary script started at $(date)}"
+echo 'Command Submitted:'
+echo $(scontrol show job "${SLURM_JOBID}" | awk -F= '/Command=/{print $2;exit;}')
+echo
+
+# load biogrids module to access analysis software
+# tools used: featurecounts, multiQC
+module load biogrids/latest
+echo
+
+# print versions of tools used for documentation
+echo 'Programs used:'
+featureCounts -v
+multiqc --version
+echo
+
+#################################################
+# FEATURECOUNTS to summarize reads by gene
+#################################################
+
+# make output folder
+mkdir -p "${OUTDIR}/featurecounts"
+
+echo 'Summarizing genecounts from:'
+echo "${OUTDIR}/deduplicated_bam"
+echo
+
+# run featurecounts to summarize exon counts for each gene
+# note that "exon" encompasses 5'UTR, CDS, and 3' UTR
+# read must match the correct strand of the genefin
+# will use all deduplicated bam files (discard those with no reads and those without indices)
+featureCounts -T ${SLURM_CPUS_PER_TASK} -t exon -g gene_id -s 1 -a "${GTFFILE}" \
+  -o "${OUTDIR}/featurecounts/genecounts.txt" \
+  $(find "${OUTDIR}/deduplicated_bam" -maxdepth 1 -type f -size +0 -name "*.dedup.bam" ! -name "*TSONaN*" ! -name "*Undetermined*")
+echo
+
+#################################################
+# MULTIQC to generate aggregate report on pipeline
+#################################################
+
+# make output folder
+mkdir -p "${OUTDIR}/multiqc"
+
+# make custom multiqc config file
+CUSTOM_CONFIG="${OUTDIR}/multiqc/multiqc_config.yaml"
+cat >"${CUSTOM_CONFIG}" <<'EOF'
+fn_ignore_files:
+  - "LoadGenome*"
+  - "RemoveGenome*"
+  - "*TSONaN*"
+  - "*Undetermined*"
+sample_names_ignore:
+  - "*TSONaN*"
+  - "*Undetermined*"
+module_order:
+  - cutadapt:
+      name: "Cutadapt (demultiplexing)"
+      anchor: "cutadapt_demultiplexing"
+      info: "This section of the report shows the demultiplexing results from cutadapt"
+      target: ""
+      path_filters:
+        - "*.cutadapt_demulti.log"
+  - fastqc:
+      name: "FastQC (raw)"
+      anchor: "fastqc_raw"
+      info: "This section of the report shows FastQC results before trimming."
+      target: ""
+      path_filters:
+        - "*.raw_fastqc.zip"
+  - cutadapt:
+      name: "Cutadapt (trimming)"
+      anchor: "cutadapt_trimming"
+      info: "This section of the report shows the trimming results from cutadapt"
+      target: ""
+      path_filters:
+        - "*.cutadapt.log"
+  - fastqc:
+      name: "FastQC (trimmed)"
+      anchor: "fastqc_trimmed"
+      info: "This section of the report shows FastQC results after trimming."
+      target: ""
+      path_filters:
+        - "*.trimmed_fastqc.zip"
+  - star
+  - samtools:
+      name: "Samtools (aligned)"
+      anchor: "samtools_aligned"
+      info: "This section of the report shows samtools statistics before deduplication"
+      target: ""
+      path_filters:
+        - "*.stats.aligned.out"
+  - samtools:
+      name: "Samtools (dedup)"
+      anchor: "samtools_dedup"
+      info: "This section of the report shows samtools statistics after deduplication"
+      target: ""
+      path_filters:
+        - "*.stats.dedup.out"
+  - rseqc
+  - qualimap
+  - featureCounts
+remove_sections:
+  - cutadapt_demultiplexing_cutadapt_trimmed_sequences
+table_columns_visible:
+  Cutadapt (demultiplexing):
+    percent_trimmed: False
+  Samtools (aligned):
+    error_rate: False
+    reads_mapped_percent: False
+    raw_total_sequences: False
+    non-primary_alignments: False
+    reads_mapped: False
+  Samtools (dedup):
+    error_rate: False
+    reads_mapped_percent: False
+    raw_total_sequences: False
+    non-primary_alignments: False
+    reads_mapped: False
+  FastQC (raw):
+    percent_duplicates: False
+    percent_gc: False
+  FastQC (trimmed):
+    avg_sequence_length: True
+    percent_duplicates: False
+  QualiMap:
+    reads_aligned: False
+table_columns_placement:
+  FastQC (raw):
+    total_sequences: 100
+  Cutadapt (trimming):
+    percent_trimmed: 200
+  FastQC (trimmed):
+    total_sequences: 300
+    avg_sequence_length: 310
+    percent_gc: 320
+  STAR:
+    uniquely_mapped: 400
+    uniquely_mapped_percent: 410
+  featureCounts:
+    Assigned: 500
+    percent_assigned: 510
+  QualiMap:
+    5_3_bias: 600
+table_columns_name:
+  FastQC (raw):
+    total_sequences: "Raw Reads"
+  FastQC (trimmed):
+    total_sequences: "Trimmed Reads"
+EOF
+
+# run multiqc on output directory, overwrite previous reports
+multiqc "${OUTDIR}" -o "${OUTDIR}/multiqc" -c "${CUSTOM_CONFIG}" -f -z --interactive
+
+# report the time
+echo
+SCRIPT_DURATION=$SECONDS
+echo "Script completed at $(date)"
+echo "Total script duration was: $(printf '%02dh:%02dm:%02ds\n' $(($SCRIPT_DURATION/3600)) $(($SCRIPT_DURATION%3600/60)) $(($SCRIPT_DURATION%60)))"
+
+##### END OF SCRIPT #####
---
+++ smart3seq_wrapper.sh
@@ -0,0 +1,130 @@
+#! /bin/bash
+
+#SBATCH -c 1 							# request 1 cores for job
+#SBATCH -t 0-00:05						# job will run for max 5 minutes
+#SBATCH -p short						# use the short partition
+#SBATCH --mem=100M						# request 100 Mib memory for job
+#SBATCH -o smart3seq_wrapper_%j.out		# name the slurm log like this
+#SBATCH --mail-type=FAIL				# send an email on job failure
+
+
+#################################################
+# SMART3SEQ WRAPPER SCRIPT
+# this script captures input from the user and submits additional scripts to process smart3seq data
+#################################################
+
+
+#################################################
+# SETUP
+#################################################
+
+# define a function to display help information for the script
+Help()
+{
+	echo 'Processes smart3seq fastq files'
+	echo 'Performs demultiplexing, trimming, alignment, UMI-deduplication, and some quality control checks'
+	echo 'Designed for running on a slurm scheduling node'
+	echo
+	echo 'Required parameters:'
+	echo '-i   input directory containing fastq.gz files'
+	echo '-o   output directory to put the generated files'
+	echo '-t   TSO barcode file (in fasta format) containing index names and sequences'
+	echo
+	echo 'Syntax: sbatch smart3seq_wrapper.sh -i [input_dir] -o [output_dir] -t [barcode_file]'
+	echo
+}
+
+
+# get the input options from the command line arguments
+while getopts ":i:o:t:h" flag; do
+	case "${flag}" in
+		i) INDIR=${OPTARG} ;;
+		o) OUTDIR=${OPTARG} ;;
+		t) TSO_FILE=${OPTARG} ;;
+		h) Help; exit ;;
+		:) echo "$0: Must supply an argument to -${OPTARG}"; exit 1;;
+		?) echo "Invalid option: -${OPTARG}"; Help; exit 1 ;;
+	esac
+done
+shift "$(($OPTIND -1))"
+
+# convert the obtained paths to absolute paths
+INDIR=$(realpath "${INDIR}")
+OUTDIR=$(realpath "${OUTDIR}")
+TSO_FILE=$(realpath "${TSO_FILE}")
+
+
+# hardcode location of genome directory (generated previously using STAR genomeGenerate)
+GENOMEDIR='/n/data1/hms/microbiology/jost/lab/genomes/human/gencode/star_index/'
+# hardcode location of downloaded GTF reference file
+GTFFILE='/n/data1/hms/microbiology/jost/lab/genomes/human/gencode/gencode.v38.annotation.gtf'
+# hardcode location of downloaded BED reference file
+BEDFILE='/n/data1/hms/microbiology/jost/lab/nolan/test/gencode.v38.annotation2.bed'
+
+
+# find the source of the other scripts we need to run
+# they should be placed in the same directory as this script
+if [ -n "${SLURM_JOB_ID}" ];  then
+	# check the original location through scontrol and $SLURM_JOB_ID
+	SCRIPT_PATH=$(dirname $(realpath $(scontrol show job "${SLURM_JOBID}" | awk -F= '/Command=/{print $2}'| awk '{print $1}')))
+else
+	# otherwise: started with bash. Get the real location. 
+	# NOTE: trying to perform analysis without slurm has not been tested
+	SCRIPT_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+fi
+
+
+#################################################
+# MAIN PROGRAM
+#################################################
+
+echo "Submitting smart3seq analysis job at $(date)"
+echo
+echo -e "Analyzing fastq.gz files in: \t ${INDIR}"
+echo -e "Processed files will be placed in: \t ${OUTDIR}"
+echo -e "Demultiplexing using barcodes in: \t ${TSO_FILE}"
+echo -e "Aligning using genome directory in: \t ${GENOMEDIR}"
+echo -e "Using GTF file in: \t ${GTFFILE}"
+echo -e "Using BED file in: \t ${BEDFILE}"
+echo -e "Using scripts located in: \t ${SCRIPT_PATH}"
+echo
+
+# make a directory to store the slurm logs
+mkdir -p "${OUTDIR}/slurmlogs"
+
+# count the number of input fastq files
+NUM_FILES=$(find "${INDIR}" -maxdepth 1 -type f -size +0 -name '*.fastq.gz' -print0 | grep -zc .)
+echo "${NUM_FILES} fastq.gz files found"
+
+# submit the processing script and capture the jobid
+echo "Submitting processing script as an array of ${NUM_FILES} jobs..."
+echo 'Command submitted:'
+
+# start xtrace mode to echo the next command before executing
+set -x
+PROCESSING_JOBID=$(sbatch --parsable -p short -c 10 -t 0-06:00 --mem=50G --mail-type=FAIL,ARRAY_TASKS \
+	--array=1-"${NUM_FILES}" -o "${OUTDIR}/slurmlogs/smart3seq_processing_%A_%a.out" \
+	"${SCRIPT_PATH}/smart3seq_processing.sh" \
+	-i "${INDIR}" -o "${OUTDIR}" -t "${TSO_FILE}" \
+	-s "${GENOMEDIR}" -g "${GTFFILE}" -b "${BEDFILE}")
+set +x
+echo "Submitted batch job ${PROCESSING_JOBID}"
+echo
+
+
+# submit the summary script to run after the processing script finishes
+echo "Submitting summary script to begin after completion of jobid: ${PROCESSING_JOBID}"
+echo 'Command submitted:'
+
+# start xtrace mode to echo the next command before executing
+set -x
+sbatch -p short -c 4 -t 0-01:00 --mem=10G --mail-type=FAIL,END \
+	--dependency=afterok:"${PROCESSING_JOBID}" -o "${OUTDIR}/slurmlogs/smart3seq_summary_parallel_%j.out" \
+	"${SCRIPT_PATH}/smart3seq_summary.sh" \
+	-o "${OUTDIR}" -g "${GTFFILE}"
+set +x
+echo
+
+echo 'smart3seq job submission complete'
+
+##### END OF SCRIPT #####
