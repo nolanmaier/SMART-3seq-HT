9f001c9a0b4194b24dc00321ac5a683c4b6ad83c
nolanmaier <91697527+nolanmaier@users.noreply.github.com>
2023-06-22 20:46:25 -0400

    Add files via upload

---
+++ barcode2fasta.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+''' 
+To demultiplex SMART-3Seqv2 data, we need a fasta file containing relevant TSO barcodes for each dT barcode
+This python script reads a csv file containing sample information and outputs the fasta files
+The csv file must have the following columns:
+	dT_Index
+	TSO_Index
+	TSO_Offset
+	TSO_Sequence
+
+
+Usage:
+python3 barcode2fasta.py sample_info.csv output_directory
+'''
+
+# import modules
+import os, sys, csv, shutil, glob
+from pathlib import Path
+
+# read sample list from path given at command line
+sample_csv = Path(sys.argv[1])
+print(f"reading sample information from \t{sample_csv}")
+
+# make a folder to store the output fasta files
+outdir = Path(sys.argv[2])
+# remove the directory and its contents if it already exists to avoid appending to an existing file
+try:
+    shutil.rmtree(outdir)
+except FileNotFoundError:
+    pass
+# make the empty output directory
+os.makedirs(outdir)
+print(f"exporting .fasta files to \t{outdir}")
+
+# initialize empty lists to hold the data
+dT_Index = []
+TSO_Index = []
+TSO_Offset = []
+TSO_Sequence = []
+# read the csv file row by row
+with open(sample_csv, mode='r', encoding='utf-8-sig') as csvfile:
+    reader = csv.DictReader(csvfile)
+    sample_count = 0
+    # extract the relevant column information to the appropriate lists
+    for row in reader:
+        dT_Index.append(int(row["dT_Index"]))
+        TSO_Index.append(int(row["TSO_Index"]))
+        TSO_Offset.append(int(row["TSO_Offset"]))
+        TSO_Sequence.append(str(row["TSO_Sequence"]))
+        sample_count += 1
+# zip the lists together so that each tuple is one entry
+data = list(zip(dT_Index, TSO_Index, TSO_Offset, TSO_Sequence))
+print(f"{sample_count} samples found")
+
+# append each entry to the appropriate fasta file
+for entry in data:
+    outname = outdir / f"dT{format(entry[0], '02')}_barcodes.fasta"
+    with open(outname, 'a') as outfile:
+        outfile.write(f">TSO{format(entry[1], '02')}_OFF{entry[2]}\n{entry[3]}\n")
+
+# count the barcodes in each fasta file
+output_file_list = sorted(glob.glob(str(outdir / 'dT*_barcodes.fasta')))
+print(f"{len(output_file_list)} fasta files written")
+for f in output_file_list:
+    f_path = Path(f)
+    with open(f_path, mode='r') as fasta_file:
+        barcode_count, remainder = divmod(len(fasta_file.readlines()), 2)
+        if remainder != 0:
+            raise AssertionError(f"wrong number of lines detected in {f_path.name}")
+        print(f"{barcode_count} barcodes in {f_path.name}")
---
+++ smart3seq_summary.sh
@@ -0,0 +1,205 @@
+#! /bin/bash
+
+#################################################
+# SMART3SEQ SUMMARY SCRIPT
+# this script runs summary tools after all the individual fastq files have been processed
+#################################################
+
+
+#################################################
+# SETUP
+#################################################
+
+# tell bash to exit immediately if something in the code fails
+set -e
+set -o pipefail
+
+# define a function to display help information for the script
+Help()
+{
+  echo 'Syntax: smart3seq_summary.sh -o [output directory] -g [gtf reference file]'
+}
+
+# get the input options from the command line arguments
+while getopts ":o:g:h" flag; do
+  case "${flag}" in
+    o) outdir=${OPTARG} ;;
+    g) gtffile=${OPTARG} ;;
+    h) Help; exit ;;
+    :) echo "$0: Must supply an argument to -${OPTARG}"; exit 1;;
+    \?) echo "Invalid option: -${OPTARG}"; Help; exit 1 ;;
+  esac
+done
+shift "$(($OPTIND -1))"
+
+
+# "SECONDS" is a special bash variable that tracks the time
+SECONDS=0
+
+# print out the command submitted to initialize this script
+echo "smart3seq summary script started at $(date)}"
+echo 'Command Submitted:'
+echo $(scontrol show job "${SLURM_JOBID}" | awk -F= '/Command=/{print $2;exit;}')
+echo
+
+cores_avail="${SLURM_CPUS_PER_TASK}"
+
+# load biogrids module to access analysis software
+# tools used: featurecounts, multiQC
+module load biogrids/latest
+echo
+
+# print versions of tools used for documentation
+echo 'Programs used:'
+featureCounts -v
+multiqc --version
+echo
+
+
+#################################################
+# FEATURECOUNTS to summarize reads by gene
+#################################################
+section_start="${SECONDS}"
+
+# make output folder
+mkdir -p "${outdir}/featurecounts"
+
+echo 'Summarizing genecounts from:'
+echo "${outdir}/deduplicated_bam"
+echo
+
+# run featurecounts to summarize exon counts for each gene
+# note that "exon" encompasses 5'UTR, CDS, and 3' UTR
+# read must match the correct strand of the genefin
+# will use all deduplicated bam files (discard those with no reads and those without indices)
+featureCounts -T "${cores_avail}" -t 'exon' -g 'gene_id' -s 1 -a "${gtffile}" \
+  -o "${outdir}/featurecounts/genecounts.tsv" \
+  $(find "${outdir}/deduplicated_bam" -maxdepth 1 -type f -size +0 -name "*.dedup.bam" ! -name "*TSONaN*" ! -name "*Undetermined*")
+echo "featureCounts finished in "$((${SECONDS}-${section_start}))" seconds"
+echo
+
+#################################################
+# MULTIQC to generate aggregate report on pipeline
+#################################################
+section_start="${SECONDS}"
+
+# make output folder
+mkdir -p "${outdir}/multiqc"
+
+# make custom multiqc config file
+custom_config="${outdir}/multiqc/multiqc_config.yaml"
+echo "Writing custom MultiQC config file to: ${custom_config}"
+cat >"${custom_config}" <<'EOF'
+fn_ignore_files:
+  - "LoadGenome*"
+  - "RemoveGenome*"
+  - "*TSONaN*"
+  - "*Undetermined*"
+sample_names_ignore:
+  - "*TSONaN*"
+  - "*Undetermined*"
+run_modules:
+  - cutadapt
+  - fastqc
+  - star
+  - umitools
+  - rseqc
+  - qualimap
+  - featureCounts
+module_order:
+  - cutadapt:
+      name: "Cutadapt (demultiplexing)"
+      anchor: "cutadapt_demultiplexing"
+      info: "This section of the report shows the demultiplexing results from cutadapt"
+      target: ""
+      path_filters:
+        - "*.cutadapt_demulti.log"
+  - cutadapt:
+      name: "Cutadapt (trimming)"
+      anchor: "cutadapt_trimming"
+      info: "This section of the report shows the trimming results from cutadapt"
+      target: ""
+      path_filters:
+        - "*.cutadapt.log"
+  - fastqc
+  - star
+  - umitools
+  - rseqc
+  - qualimap
+  - featureCounts
+sp:
+  cutadapt:
+    fn: '*.cutadapt*.log'
+  umitools:
+    fn: '*.umitools.log'
+  rseqc/bam_stat:
+    fn: '*.bamStat.txt'
+    max_filesize: 500000
+  rseqc/gene_body_coverage:
+    skip: true
+  rseqc/inner_distance:
+    skip: true
+  rseqc/junction_annotation:
+    skip: true
+  rseqc/junction_saturation:
+    skip: true
+  rseqc/read_gc:
+    skip: true
+  rseqc/read_distribution:
+    fn: '*.readDistribution.txt'
+    max_filesize: 500000
+  rseqc/read_duplication_pos:
+    skip: true
+  rseqc/infer_experiment:
+    fn: '*.inferExperiment.txt'
+    max_filesize: 500000
+  rseqc/tin:
+    skip: true
+remove_sections:
+  - cutadapt_demultiplexing_cutadapt_trimmed_sequences
+table_columns_visible:
+  Cutadapt (demultiplexing):
+    percent_trimmed: False
+  FastQC:
+    avg_sequence_length: False
+    percent_duplicates: False
+    median_sequence_length: True
+  QualiMap:
+    reads_aligned: False
+table_columns_placement:
+  Cutadapt (trimming):
+    percent_trimmed: 200
+  FastQC:
+    total_sequences: 300
+    median_sequence_length: 310
+    percent_gc: 320
+  STAR:
+    uniquely_mapped: 400
+    uniquely_mapped_percent: 410
+  UMI-tools:
+    output_reads: 500
+    percent_passing_dedup: 510
+  featureCounts:
+    Assigned: 600
+    percent_assigned: 610
+  QualiMap:
+    5_3_bias: 700
+custom_plot_config:
+  cutadapt_filtered_reads_plot:
+    cpswitch_c_active: False
+EOF
+echo
+
+# run multiqc on output directory, overwrite previous reports
+multiqc "${outdir}" -o "${outdir}/multiqc" -c "${custom_config}" -f -z
+echo
+echo "MultiQC finished in "$((${SECONDS}-${section_start}))" seconds"
+echo
+
+
+# report the time
+script_duration=$SECONDS
+echo "Script completed at $(date)"
+echo "Total script duration was: $(printf '%02dh:%02dm:%02ds\n' $(($script_duration/3600)) $(($script_duration%3600/60)) $(($script_duration%60)))"
+
+##### END OF SCRIPT #####
